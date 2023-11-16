374035177e29701a934cc4bfe1c20961d0f89ad2
domdevbox <145929043+domdevbox@users.noreply.github.com>
2023-11-16 12:22:42 +0000

    Deploying to gh-pages from @ domdevbox/tailwind-datepicker-react@c1c00aaaf0504393a0f8b968203fe618f5d6f850 🚀

---
+++ _next/static/6umO0fzlSh38lbkyY_fo6/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/745-66cf71dfc77f1a61.js","static/chunks/pages/index-0eab9b4c8ce379a7.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
